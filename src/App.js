80cc45696123c023dc89e2a70e47000ae5f57f58
KayalvizhiShanmugam <s.kayalvizhi00@gmail.com>
2023-09-20 11:11:51 +0530

    tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+//import { Alert } from "@material-ui/icons";
+import { useState } from "react";
+import Confetti from "react-confetti";
+import useWindowSize from "react-use/lib/useWindowSize";
+import Button from '@material-ui/core/Button';
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game />
     </div>
   );
 }
 
-export default App;
+export function Game() {
+  const [board, setBoard] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null
+  ]);
+  const { width, height } = useWindowSize();
+  const decideWinner = (board) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (board[a] !== null && board[a] === board[b] && board[a] === board[c]) {
+        console.log("Winner", board[a]);
+        return board[a];
+      }
+      //return null;
+    }
+
+    //board[0] != null && board[0] == board[1] && board[0] === board[2]
+  };
+
+  const winner = decideWinner(board);
+  const [isXTurn, setIsXTurn] = useState(true);
+  const handelClick = (index) => {
+    console.log(index);
+    const boardCopy = [...board];
+    if (!winner && !board[index]) {
+      boardCopy[index] = isXTurn ? "X" : "O";
+      setIsXTurn(!isXTurn);
+      setBoard(boardCopy);
+    }
+  };
+
+  const reset = () => {
+    setBoard([null, null, null, null, null, null, null, null, null]);
+    setIsXTurn(true);
+  };
+
+  return (
+    <>
+      {winner ? <Confetti width={width} height={height} /> : ""}
+      <h2 className={"game-name"}>Tic - Tac - Toe</h2>
+      <div className={"board"}>
+        {/* <Alert severity="success">{winner}</Alert> */}
+        {board.map((value, index) => (
+          <Boxes
+            value={value}
+            key={index}
+            onPlayerClick={() => handelClick(index)}
+          />
+        ))}
+      </div>
+      <div className={"game-message"}>
+        <Button variant="contained" color="secondary" onClick={reset}>
+          Restart the game
+        </Button>
+
+        <p>{winner ? "The Winner is: " + winner : ""}</p>
+        <p>{!winner ? `The Player ${isXTurn ? "X" : "O"} has to play` : ""}</p>
+      </div>
+    </>
+  );
+}
+
+const Boxes = ({ value, onPlayerClick }) => {
+  //const [pay, setPlay] = useState("");
+  // const increment =
+  const styles = {};
+  if (!!value) {
+    // not undefined
+    styles.color = value === "X" ? "green" : "red";
+  }
+  return (
+    <div className="game-box" onClick={onPlayerClick} style={styles}>
+      {value}
+    </div>
+  );
+};
